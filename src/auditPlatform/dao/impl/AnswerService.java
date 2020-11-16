756994416bbdd211f15472a67bd80ff92164d7ad
白夜 <1752228448@qq.com>
2020-11-16 21:06:42 +0800

    项目主体结构

--- src/auditPlatform/dao/impl/AnswerService.java
+++ src/auditPlatform/dao/impl/AnswerService.java
@@ -1,5 +1,84 @@
 package auditPlatform.dao.impl;
 
-public interface AnswerService {
+import auditPlatform.pojo.Answer;
 
+import java.sql.Connection;
+import java.util.List;
+
+public interface AnswerService {
+    /**
+     * 增加题解
+     * @param con
+     * @param answer
+     * @return
+     */
+    int addAnswer(Connection con, Answer answer);
+
+    /**
+     * 根据ID删除题解
+     * @param con
+     * @param id
+     * @return
+     */
+    int deleteAnswer(Connection con,int id);
+
+    /**
+     * 根据id查找题解
+     * @param con
+     * @param id
+     * @return
+     */
+    Answer findAnswerById(Connection con,int id);
+
+    /**
+     * 根据问题id查找题解(并根据数据权值从大到小排序)
+     * @param con
+     * @param problemsId
+     * @return
+     */
+    List<Answer> findAnswerByProblemsId(Connection con, int problemsId);
+    /**
+     * 根据用户id查找题解
+     * @param con
+     * @param userId
+     * @return
+     */
+    List<Answer> findAnswerByUserId(Connection con, int userId);
+
+    /**
+     * 根据题解id发布题解
+     * @param con
+     * @param id
+     * @return
+     */
+    int publishAnswer(Connection con,int id);
+
+    /**
+     * 点赞题解
+     * @param con
+     * @param id
+     * @return
+     */
+    int thumbsUpAnswer(Connection con,int id);
+    /**
+     * 题解收藏+1
+     * @param con
+     * @param id
+     * @return
+     */
+    int collectAnswer(Connection con,int id);
+    /**
+     * 题解浏览量+1
+     * @param con
+     * @param id
+     * @return
+     */
+    int viewAnswer(Connection con,int id);
+    /**
+     * 题解评论+1
+     * @param con
+     * @param id
+     * @return
+     */
+    int commentAnswer(Connection con,int id);
 }
--- src/auditPlatform/dao/impl/CommentService.java
+++ src/auditPlatform/dao/impl/CommentService.java
@@ -1,4 +1,48 @@
 package auditPlatform.dao.impl;
 
+import auditPlatform.pojo.Comment;
+
+import java.sql.Connection;
+import java.util.List;
+
 public interface CommentService {
+    /**
+     * 添加评论
+     * @param con
+     * @param comment
+     * @return
+     */
+    int addComment(Connection con, Comment comment);
+    /**
+     * 点赞评论(根据id)
+     * @param con
+     * @param id
+     * @return
+     */
+    int thumbsUpComment(Connection con,int id);
+
+    /**
+     * 根据id删除评论
+     * @param con
+     * @param id
+     * @return
+     */
+    int deleteComment(Connection con,int id);
+
+    /**
+     * 根据id查找评论
+     * @param con
+     * @param Id
+     * @return
+     */
+    Comment findCommentById(Connection con,int Id);
+
+    /**
+     * 根据问题id查找评论
+     * @param con
+     * @param problemsId
+     * @return
+     */
+    List<Comment> findCommentByProblemsId(Connection con,int problemsId);
+
 }
